fe4285cdda226c9a496f0bf3f53289860f0eca21
dejan <dejanr77@yahoo.com>
2025-09-09 19:10:51 +0200

    add footer

--- resources/views/components/layouts/frontend/app.blade.php
+++ resources/views/components/layouts/frontend/app.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.frontend.app.header :title="$title ?? null">
     <flux:main container>
         {{ $slot }}
+
+        @include('partials.frontend.footer')
     </flux:main>
 </x-layouts.frontend.app.header>
